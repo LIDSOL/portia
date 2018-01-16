40290ba61348e5c08dafc3d7d1400e9ab8c0ed79
Emilio Cabrera <emilio1625@gmail.com>
2018-01-16 14:07:18 -0600

    Se añade el proyecto de Unity

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour {
+
+    //private Vector3 startingPosition;
+    public Material inactiveMaterial;
+    public Material activeMaterial;
+    public string destination;
+    private float gazeOnTime = 2f;
+    private float timer;
+    private bool gazeAt;
+
+	// Use this for initialization
+	void Start () {
+        //startingPosition = transform.localPosition;
+        timer = 0f;
+        GetComponent<Renderer>().material = inactiveMaterial;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (gazeAt)
+        {
+            timer += Time.deltaTime;
+            if (timer >= gazeOnTime)
+            {
+                timer = 0f;
+                ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerClickHandler);
+            }
+        }
+    }
+
+    public void PointerEnter()
+    {
+        Debug.Log("Pointer Enter");
+        GetComponent<Renderer>().material = activeMaterial;
+        gazeAt = true;
+    }
+
+    public void PointerExit()
+    {
+        Debug.Log("Pointer Exit");
+        GetComponent<Renderer>().material = inactiveMaterial;
+        timer = 0f;
+        gazeAt = false;
+    }
+
+    public void PointerClick()
+    {
+        Debug.Log("Pointer Click");
+        SceneManager.LoadScene(destination);
+    }
+
+    public void QuitApp()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/SpiderScript.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SpiderScript : MonoBehaviour {
+    private NavMeshAgent agent;
+    private Animator anim;
+
+    private float timer;
+    private Vector3 nextPosition;
+    private bool gazedAt = false;
+    private int health = 100;
+
+    public float travelTime = 5f;
+    public float walkSpeed = 2f;
+    public float walkableRadius = 5f;
+    public bool startRandomPosition = false;
+
+    // Use this for initialization
+	void Start ()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        SetHealth(health);
+        agent.transform.position = Random.insideUnitSphere * walkableRadius;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer += Time.deltaTime;
+        if (timer > travelTime)
+        {
+            if (health > 0)
+            {
+                timer = 0.0f;
+                nextPosition = Random.insideUnitSphere * walkableRadius;
+                //nextPosition.y = 0f;
+                agent.SetDestination(nextPosition);
+                anim.SetFloat("walkSpeed", walkSpeed);
+            }
+        }
+
+        if (gazedAt) {
+            incHealth(-1);
+        }
+
+        if (Mathf.Abs(agent.velocity.y) > 0.2f)
+        {
+            anim.SetBool("jump", true);
+        }
+        else
+        {
+            anim.SetBool("jump", false);
+        }
+
+        if (agent.remainingDistance == 0f || (Mathf.Abs(agent.velocity.x) < 0.1f && Mathf.Abs(agent.velocity.z) < 0.1f))
+        {
+            anim.SetBool("onMove", false);
+        }
+        else
+        {
+            anim.SetBool("onMove", true);
+        }
+
+        if (health == 0) {
+            Destroy(gameObject, 1.5f);
+        }
+    }
+
+    public void SetGazedAt(bool gazedAt)
+    {
+        this.gazedAt = gazedAt;
+    }
+
+    public void SetHealth(int value)
+    {
+        health = value;
+        if (health <= 0) health = 0;
+        this.anim.SetInteger("health", health);
+    }
+
+    public void incHealth(int value = 1)
+    {
+        health += value;
+        if (health <= 0) health = 0;
+        this.anim.SetInteger("health", health);
+    }
+
+}
