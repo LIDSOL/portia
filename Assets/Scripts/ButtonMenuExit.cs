41461fe20a57e9f85e59957180190059cda98048
luisfi <luisunamfi@gmail.com>
2018-06-26 13:57:46 -0500

    Se soluciona error en el botón Salir de la escena del menú principal

---
+++ Assets/Scripts/ButtonMenuExit.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ButtonMenuExit : MonoBehaviour
+{
+
+    public Material HighLightMaterial;
+    public float GazeOnTime = 1.0f;
+
+    private Material NormalMaterial;
+    private BoxCollider boxCollider;
+    private Renderer MyRenderer;
+    private float Timer;
+    private bool GazeAt;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.MyRenderer = GetComponent<Renderer>();
+        this.NormalMaterial = this.MyRenderer.material;
+        this.Timer = 0.0f;
+        this.GazeAt = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GazeAt)
+        {
+            Timer += Time.deltaTime;
+            if (Timer >= GazeOnTime)
+            {
+                Application.Quit();
+            }
+        }
+    }
+
+    public void HighLight()
+    {
+        MyRenderer.material = HighLightMaterial;
+        GazeAt = true;
+    }
+
+    public void NormalRender()
+    {
+        MyRenderer.material = NormalMaterial;
+        GazeAt = false;
+        Timer = 0.0f;
+    }
+
+}
